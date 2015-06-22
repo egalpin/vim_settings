d7dee6efa2d1b4727ce19f95f5eded04b2105358
Evan Galpin <evan.galpin@greenbricklabs.com>
2015-06-22 14:18:09 -0400

    Updated setup script

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#/bin/bash
 #TODO automate phpctags, YouCompleteMe, jsctags, tern
 declare -r RED='\033[0;31m'
 declare -r GREEN='\033[0;32m'
@@ -14,16 +14,16 @@ fi
 reportStatus ()
 {
     if [ $? -eq 0 ]; then
-        echo "${GREEN}Success!${NC}"
+        echo -e "${GREEN}Success!${NC}"
     else
-        echo "${RED}Failed!${NC}"
+        echo -e "${RED}Failed!${NC}"
     fi
 }
 
 installCtags ()
 {
     cwd=$( pwd )
-    echo "${CYAN}Configuring and installing ~/.vim/ctags-5.8 for Tagbar (requires root privileges)${NC}"
+    echo -e "${CYAN}Configuring and installing ~/.vim/ctags-5.8 for Tagbar (requires root privileges)${NC}"
     cd ~/.vim/ctags_src
     tar xzf ctags-5.8.tar.gz
     cd ctags-5.8
@@ -33,7 +33,7 @@ installCtags ()
         sudo ./configure > /dev/null
     fi
     if [ $? -eq 0 ]; then
-        echo "${CYAN}Ctags configured successfully, attempting to install...${NC}"
+        echo -e "${CYAN}Ctags configured successfully, attempting to install...${NC}"
         sleep 2
         if [ -n "$win_chk" ]; then
             make > /dev/null 2>&1 && make install > /dev/null
@@ -41,10 +41,10 @@ installCtags ()
             sudo make > /dev/null 2>&1 && sudo make install > /dev/null
         fi
         if [ $? -eq 0 ]; then
-            echo "${GREEN}Ctags successfully installed to $( which ctags )${NC}"
+            echo -e "${GREEN}Ctags successfully installed to $( which ctags )${NC}"
         else
-            echo "${RED}Ctags failed to install"
-            echo -n "Continue with setup? [y|n]:  ${NC}"
+            echo -e "${RED}Ctags failed to install"
+            echo -e -n "Continue with setup? [y|n]:  ${NC}"
             read pass
             if [ "$pass" = 'y' -o "$pass" = 'Y' ]; then
                 continue
@@ -53,8 +53,8 @@ installCtags ()
             fi
         fi
     else
-        echo "${RED}Ctags failed to configure. Tagbar will not work until ctags is configured correctly${NC}"
-        echo -n "Continue with setup? [y|n]:  "
+        echo -e "${RED}Ctags failed to configure. Tagbar will not work until ctags is configured correctly${NC}"
+        echo -e -n "Continue with setup? [y|n]:  "
         read pass
         if [ "$pass" = 'y' -o "$pass" = 'Y' ]; then
             continue
@@ -67,10 +67,9 @@ installCtags ()
 
 installYCM ()
 {
-    echo "${CYAN}Configuring and installing YouCompleteMe${NC}"
+    echo -e "${CYAN}Configuring and installing YouCompleteMe${NC}"
     cwd=$( pwd )
     cd ~/.vim/bundle/YouCompleteMe
-    git submodule update --init --recursive
     bash install.sh --clang-completer
     reportStatus
     eval cd $cwd
@@ -78,14 +77,14 @@ installYCM ()
 
 installJsctags ()
 {
-    echo "${CYAN}Configuring and installing YouCompleteMe${NC}"
+    echo -e "${CYAN}Configuring and installing YouCompleteMe${NC}"
     sudo npm install -g git+https://github.com/ramitos/jsctags.git
     reportStatus
 }
 
 installPhpctags ()
 {
-    echo "${CYAN}Configuring and installing tagbar-phpctags${NC}"
+    echo -e "${CYAN}Configuring and installing tagbar-phpctags${NC}"
     cwd=$( pwd )
     cd ~/.vim/bundle/tagbar-phpctags
     make
@@ -95,7 +94,7 @@ installPhpctags ()
 
 installTernForVim ()
 {
-    echo "${CYAN}Configuring and installing tern_for_vim${NC}"
+    echo -e "${CYAN}Configuring and installing tern_for_vim${NC}"
     cwd=$( pwd )
     cd ~/.vim/bundle/tern_for_vim
     npm install
@@ -105,26 +104,25 @@ installTernForVim ()
 
 installNode ()
 {
-    echo "${CYAN}Configuring and installing Node and NPM${NC}"
-    if [[ "$OS_CHK" = true ]] && [ "$OS_NAME" -eq "Darwin" ]; then
+    echo -e "${CYAN}Configuring and installing Node and NPM${NC}"
+    if [[ "$OS_CHK" = true ]] && [[ "$OS_NAME" == "Darwin" ]]; then
         # handle Mac brew, node, and npm install
         ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
         brew install node
         reportStatus
-    elif [[ "$OS_CHK" = true ]] && [ "$OS_NAME" -eq "Linux" ]; then
+    elif [[ "$OS_CHK" = true ]] && [[ "$OS_NAME" == "Linux" ]]; then
         # handle Linux  node and npm install
-        sudo apt-get install nodejs
-        sudo apt-get install npm
+        sudo apt-get -y install nodejs npm
         reportStatus
     else
-        echo "${RED}Your OS does not support node${NC}"
-        echo "${RED}Install cannot be completed${NC}"
+        echo -e "${RED}Your OS does not support node${NC}"
+        echo -e "${RED}Install cannot be completed${NC}"
         exit 1
     fi
 }
 
 if [ "$(pwd)" != "$HOME" ]; then
-    echo "${CYAN}Saving ~/.vim, ~/.vimrc, and ~/fonts to .bkp${NC}"
+    echo -e "${CYAN}Saving ~/.vim, ~/.vimrc, and ~/fonts to .bkp${NC}"
     # Save any existing vim configs
     [ -d ~/.vim ] && mv ~/.vim ~/.vim.bkp
     [ -f ~/.vimrc ] && mv ~/.vimrc ~/.vimrc.bkp
@@ -132,7 +130,7 @@ if [ "$(pwd)" != "$HOME" ]; then
 fi
 
 if [ "$(pwd)" != "$HOME" ]; then
-    echo "${CYAN}Copying .vim, .vimrc, and fonts to ~${NC}"
+    echo -e "${CYAN}Copying .vim, .vimrc, and fonts to ~${NC}"
     # Copy new Vim configs
     cp -rf .vim ~/.vim
     cp -f .vimrc ~/.vimrc
@@ -146,7 +144,7 @@ installJsctags
 installPhpctags
 installTernForVim
 
-echo "${CYAN}Next steps: set the terminal font to a powerline font; check YouCompleteMe, jsctags, phpctags on GitHub to verify installation${NC}"
-echo "${GREEN}Done!${NC}"
+echo -e "${CYAN}Next steps: set the terminal font to a powerline font; check YouCompleteMe, jsctags, phpctags on GitHub to verify installation${NC}"
+echo -e "${GREEN}Done!${NC}"
 
 exit 0
