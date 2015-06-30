270e5a5bcdd8982832547ed909c785694f73f440
Evan Galpin <evan.galpin@greenbricklabs.com>
2015-06-30 14:07:25 -0400

    Added sudo to copy apt-vim to bin

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cp .vimrc ~/.vimrc
 git clone https://github.com/egalpin/apt-vim.git
-cp apt-vim/apt-vim /usr/local/bin
+sudo cp apt-vim/apt-vim /usr/local/bin
 apt-vim init
 cp vim_config.json ~/.vimpkg/vim_config.json
 apt-vim install -y
