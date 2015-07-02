81c1b480508472a58e6469eb579dd2b38c61c733
Evan Galpin <evan.galpin@greenbricklabs.com>
2015-07-02 10:56:27 -0400

    Added checking to apt-vim and backup of vim_config.json

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 cp .vimrc ~/.vimrc
-git clone https://github.com/egalpin/apt-vim.git
-apt-vim init
+if [ -z $(which apt-vim) ]; then
+    git clone https://github.com/egalpin/apt-vim.git
+    apt-vim init
+fi
+# Backup the old vim_config
+if [ -f ~/.vimpkg/vim_config.json ]; then
+    mv ~/.vimpkg/vim_config.json ~/.vimpkg/vim_config.json.bak
+fi
 # Overwrite the default vim_config
 cp vim_config.json ~/.vimpkg/vim_config.json
 apt-vim install -y
