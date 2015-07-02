bfdaee95d4e4f277345a630b4daf4b192e1e86b9
Evan Galpin <evan.galpin@greenbricklabs.com>
2015-07-02 10:52:18 -0400

    Improved vim_config.json

--- vimSetup.sh
+++ vimSetup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 cp .vimrc ~/.vimrc
 git clone https://github.com/egalpin/apt-vim.git
-sudo cp apt-vim/apt-vim /usr/local/bin
 apt-vim init
+# Overwrite the default vim_config
 cp vim_config.json ~/.vimpkg/vim_config.json
 apt-vim install -y
