df1abadac682628e9e19e230c3f3e9e266110621
Evan Galpin <evan.galpin@greenbricklabs.com>
2015-11-27 17:44:33 -0500

    updated vimSetup.sh

--- vimSetup.sh
+++ vimSetup.sh
@@ -19,23 +19,14 @@ cp ./vim_config.json ~/.vimpkg/vim_config.json
 # Execute apt-vim init
 cd ${HOME}/apt-vim
 sudo python - <<EOF
-import imp, os, json
+import imp, os
 HOME = os.path.expanduser("~")
 APT_VIM_DIR = os.path.abspath(os.path.join(HOME, 'apt-vim'))
-SCRIPT_ROOT_DIR = os.path.abspath(os.path.join(HOME, '.vimpkg'))
-BIN_DIR = os.path.abspath(os.path.join(SCRIPT_ROOT_DIR, 'bin'))
-VIM_CONFIG_PATH = os.path.abspath(os.path.join(SCRIPT_ROOT_DIR, 'vim_config.json'))
 os.environ['PATH'] += os.pathsep + BIN_DIR
 os.chdir(APT_VIM_DIR)
 
 aptvim = imp.load_source("aptvim", "./apt-vim")
-global ASSUME_YES
-ASSUME_YES = True
-
-global VIM_CONFIG
-json_file = open(VIM_CONFIG_PATH).read()
-VIM_CONFIG = json.loads(json_file)
-print(VIM_CONFIG)
-aptvim.handle_install(None, None, None)
+apt_vim = aptvim.aptvim(ASSUME_YES=True)
+apt_vim.handle_install(None, None, None)
 EOF
 cd $start_dir
